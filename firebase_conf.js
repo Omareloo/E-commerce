75109575f205852bdb406916a4d16470db706c6a
Omar Ali <omarh9392@gmail.com>
2025-07-25 14:28:15 +0300

    first edits

--- firebase_conf.js
+++ firebase_conf.js
@@ -1,7 +1,15 @@
-/*
+const firebaseConfig = {
+    apiKey: "AIzaSyD9x4LGO8jrIShzyguY8mJ_CQ5lO2cm05Q",
+    authDomain: "e-commerce-21ae3.firebaseapp.com",
+    projectId: "e-commerce-21ae3",
+    storageBucket: "e-commerce-21ae3.firebasestorage.app",
+    messagingSenderId: "387811424080",
+    appId: "1:387811424080:web:5ea8bc38f47a232ebfde55",
+    measurementId: "G-XKBSFE7V2T"
+  };
 
-firebase-config.js
 
-*/
-
-console.log("firebase-config.js");
+  firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
